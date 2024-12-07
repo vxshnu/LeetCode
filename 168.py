42b0fd6d8f63e62126d938393dc4be941c923033
vxshnu <vishnunarayanan8690@gmail.com>
2024-12-07 23:54:28 +0530

    168

--- 168.py
+++ 168.py
@@ -4,20 +4,11 @@ class Solution:
             return chr(n + 64)  
 
     def convertToTitle(self, columnNumber: int) -> str:
-        x = columnNumber // 26
-        y = columnNumber % 26
-        z = x
-        firstalpha = ""
-        while z > 0:
-            r = z % 26
-            if r == 0:  
-                firstalpha = self.number_to_letter(26) + firstalpha
-                z = (z // 26) - 1 
-            else:
-                firstalpha = self.number_to_letter(r) + firstalpha
-                z = z // 26
-            z = z//26
-        
-        lastalpha = self.number_to_letter(y)
-        converted = firstalpha+lastalpha
-        return converted
+        x = columnNumber 
+        output = ""
+        while x > 0:
+            x = x - 1
+            r = x % 26
+            output = chr(ord('A') + r) + output
+            x = x//26
+        return output
