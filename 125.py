a11c941f56524730f335426b0118749cf35d8135
vxshnu <vishnunarayanan8690@gmail.com>
2024-12-10 22:27:24 +0530

    125

---
+++ 125.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        i = 0
+        l = len(s)-1
+        s = s.lower()
+        while i < l/2:
+            if s[i].isalnum() and s[l-i].isalnum():
+                if s[i] != s[l-i]:
+                    return False
+                print(s[i],s[l-i])
+                i = i+1
+            else:
+                if not s[i].isalnum():
+                    s = s[:i] + s[i+1:]
+                else:
+                    index_to_remove = l - i
+                    s = s[:index_to_remove] + s[index_to_remove+1:]
+        return True
